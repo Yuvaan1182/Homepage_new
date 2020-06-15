298925d724bf0cc84e0164484a5a7511aaf01c34
Jakub Galinski <thougless@gmail.com>
2020-06-15 16:42:16 +0200

    fixed dark theme v2, fixed hamburger

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 import "../scss/main-style.scss";
-import bwBackground from "../assets/img/landscape_forest_BW.jpg"
+// import bwBackground from "dist/img/landscape_forest_BW.jpg"
 
 
 //-----------------Dark/Light Mode
+
+ 
+   let imgBw = window.getComputedStyle(document.querySelector(".dupa")).getPropertyValue('background-image');
+   let img =  window.getComputedStyle(document.querySelector("body")).getPropertyValue('background-image');
 let modeSwitch = document.querySelector(".switchInput");
 if (modeSwitch) {
   modeSwitch.addEventListener("input", () => {
@@ -11,14 +15,14 @@ if (modeSwitch) {
     if (modeSwitch.value == "true") {
       bodyStyle.setProperty(
         "--background-img",
-        "url('../../public/landscape_forest_BW.jpg')"
+      imgBw
       );
       navColor.setProperty( "--nav-color", "#d86c46")
       modeSwitch.value = false;
     } else if ((modeSwitch.value = "false")) {
       bodyStyle.setProperty(
         "--background-img",
-        "url('../../public/landscape_forest.jpg')"
+       img
       );
       navColor.setProperty( "--nav-color", "black")
       modeSwitch.value = true;
@@ -35,7 +39,7 @@ if (buttonChange) {
     nav.classList.toggle("navigation__open");
   });
 }
-
+console.log('duupa')
 if (window) {
   window.addEventListener("resize", () => {
     if (
