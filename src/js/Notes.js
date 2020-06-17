b133fea506162094f6c169ce96115dd58fe7b544
Jakub Galinski <thougless@gmail.com>
2020-06-18 00:32:57 +0200

    adding new notes mechanism

--- src/js/Notes.js
+++ src/js/Notes.js
@@ -2,32 +2,42 @@ import "../scss/style-notes.scss";
 import singleNote from "../pages/Note.html";
 
 let Notes = localStorage.getItem("Notes");
-let NotesArr = []
-let emptyNote = {title : 'Title', noteText : 'Note text'}
+let NotesArr = [];
+let emptyNote = { title: "Title", noteText: "Note text", id: 0 };
+var z = document.createRange().createContextualFragment(singleNote);
+document.querySelector("#notebook").appendChild(z);
+const newNote = () => {
+  let newNote = document.querySelector("#note-0").cloneNode(true);
+  newNote.style.display = "inline";
+  newNote.id = "note-" + document.querySelector("#notebook").childElementCount;
+  newNote.childNodes[1].addEventListener("click", (e) => {
+    newNote.style.display = "none";
+  });
+  document.querySelector("#notebook").appendChild(newNote);
+};
+
 if (Notes == null) {
   Notes = [emptyNote];
   localStorage.setItem("Notes", JSON.stringify(Notes));
 } else {
- NotesArr = JSON.parse(localStorage.getItem("Notes"))
-document.querySelector('#note-1').querySelector('h2').innerHTML = NotesArr[0].title
-document.querySelector('#note-1').querySelector('p').innerHTML = NotesArr[0].noteText
+  NotesArr = JSON.parse(localStorage.getItem("Notes"));
+  console.log(typeof NotesArr);
+  newNote();
+  NotesArr.forEach((elem, idx) => {
+    document.querySelector(`#note-${idx + 1}`).querySelector("h2").innerHTML =
+      elem.title;
+    document.querySelector(`#note-${idx + 1}`).querySelector("p").innerHTML =
+      elem.noteText;
+  });
 }
 // console.log(NotesArr)
 let buttonAdd = document.querySelector(".buttonAdd");
 
 if (buttonAdd) {
   buttonAdd.addEventListener("click", () => {
-    console.log("add new note");
-    let newNote = document.querySelector("#note-0").cloneNode(true);
-    newNote.style.display = "inline";
-    newNote.id =
-      "note-" + document.querySelector("#notebook").childElementCount;
-      console.log(newNote)
-    newNote.childNodes[1].addEventListener("click", (e) => {
-        newNote.style.display='none'
-    });
-    document.querySelector("#notebook").appendChild(newNote);
-
+    newNote();
+    // document.querySelector('#notebook').lastChild
+    appendEditListener()
   });
 }
 
@@ -36,7 +46,7 @@ if (deleteAllButton) {
   deleteAllButton.addEventListener("click", () => {
     Array.from(document.getElementsByClassName("note-paper")).forEach(
       (elem) => {
-        elem.style.display = "none";
+        elem.remove();
       }
     );
   });
@@ -51,26 +61,25 @@ if (note) {
   });
   // let deleteNote = (elem) => {elem.style.display ='none'};
 }
+const appendEditListener = () => {
+ let editSave = document.querySelectorAll(".editSave");
+ if (editSave) {
+   editSave.forEach((element) => {
+     element.addEventListener("click", () => {
+       let notePaper = element.closest(".note-paper")
+       if(notePaper.getAttribute('contenteditable') == 'true'){    // TRIGGERING THE SAVE EVENT
 
-let editSave = document.querySelectorAll(".editSave");
-console.log(NotesArr)
-if (editSave) {
-  editSave.forEach((element) => {
-    element.addEventListener("click", () => {
-      let notePaper = element.closest(".note-paper")
-      if(notePaper.getAttribute('contenteditable') == 'true'){    // TRIGGERING THE SAVE EVENT
-     
-        NotesArr[0].title = notePaper.querySelector('h2').innerHTML
+         NotesArr[0].title = notePaper.querySelector('h2').innerHTML
         NotesArr[0].noteText = notePaper.querySelector('p').innerHTML
         console.log(NotesArr)
-        localStorage.setItem("Notes", JSON.stringify(NotesArr));
-         notePaper.setAttribute('contenteditable','false')
-         notePaper.querySelector('.editSave').innerHTML = "Edit"
+       localStorage.setItem("Notes", JSON.stringify(NotesArr));
+        notePaper.setAttribute('contenteditable','false')
+       notePaper.querySelector('.editSave').innerHTML = "Edit"
         } else {notePaper.setAttribute('contenteditable','true')
         notePaper.querySelector('.editSave').innerHTML = "Save"
       }
-     
-    });
-  });
-}
 
+     });  
+    });
+ }
+}
