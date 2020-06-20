71c79f69cf2125b1b84c65f45ae0f1a9680909d5
Jakub Galinski <thougless@gmail.com>
2020-06-20 21:38:39 +0200

    getting data with refresh works for notes-grid

--- src/js/notes-grid.js
+++ src/js/notes-grid.js
@@ -4,32 +4,31 @@ import singleNote from "../pages/Note.html";
 
 let Notes = localStorage.getItem("Notes");
 let NotesArr = [];
-let emptyNote = { title: "Title", noteText: "Note text", id: 0 };
+let emptyNote = { title: "Title", noteText: "Note text"};
 const firstNote = () => {
   var z = document.createRange().createContextualFragment(singleNote);
   document.querySelector("#notebook").appendChild(z);
 };
 
-const newNote = () => {    //   COPY NOTE-0 AND CREATE NEW NOTE
+const newNote = () => {
+  //   COPY NOTE-0 AND CREATE NEW NOTE
   let newNote = document.createRange().createContextualFragment(singleNote);
   document.querySelector("#notebook").appendChild(newNote);
-  
-  document.querySelector("#notebook").appendChild(newNote);
-  console.log(newNote)
-  console.log( document.querySelector("#notebook").lastElementChild)
-  document.querySelector("#notebook").lastChild.id = "note-" + (document.querySelector("#notebook").childElementCount-1);
+  document.querySelector("#notebook").lastElementChild.id =
+    "note-" + (document.querySelector("#notebook").childElementCount - 1);
+
 };
-console.log(NotesArr)
+console.log(NotesArr);
 if (Notes == null) {
-  console.log('jaja')
+  console.log("jaja");
   firstNote();
-  NotesArr.push(emptyNote)
+  NotesArr.push(emptyNote);
+  NotesArr.push({title:"dupa", noteText: "jajajajaja"})
   localStorage.setItem("Notes", JSON.stringify(NotesArr));
 } else {
-    console.log(localStorage.getItem("Notes"))
+  console.log(localStorage.getItem("Notes"));
   NotesArr = JSON.parse(localStorage.getItem("Notes"));
-
-
+console.log(NotesArr)
   NotesArr.forEach((elem, idx) => {
     newNote();
     document.querySelector(`#note-${idx}`).querySelector("h2").innerHTML =
@@ -37,7 +36,6 @@ if (Notes == null) {
     document.querySelector(`#note-${idx}`).querySelector("p").innerHTML =
       elem.noteText;
   });
-
 }
 // console.log(NotesArr)
 let buttonAdd = document.querySelector(".buttonAdd");
@@ -77,15 +75,17 @@ const appendEditListener = () => {
   if (editSave) {
     editSave.forEach((element) => {
       element.addEventListener("click", () => {
+        console.log('onclick starts')
         let notePaper = element.closest(".note-paper");
         if (notePaper.getAttribute("contenteditable") == "true") {
           // TRIGGERING THE SAVE EVENT
-
-          updateNote(notePaper.id, NotesArr);
+          console.log(NotesArr)
+           updateNote(notePaper.id,NotesArr);
 
           notePaper.setAttribute("contenteditable", "false");
           notePaper.querySelector(".editSave").innerHTML = "Edit";
         } else {
+          console.log(NotesArr)
           notePaper.setAttribute("contenteditable", "true");
           notePaper.querySelector(".editSave").innerHTML = "Save";
         }
@@ -99,7 +99,7 @@ const updateNote = (id, NotesArr) => {
   console.log(NotesArr);
   let notePaper = document.getElementById(id);
   let arrId = id.slice(5);
-  console.log("console log: " + NotesArr[arrId].title);
+  // console.log("console log: " + NotesArr[arrId].title);
   if ((NotesArr[arrId] = null))
     NotesArr[arrId].title = notePaper.querySelector("h2").innerHTML;
   NotesArr[arrId].noteText = notePaper.querySelector("p").innerHTML;
